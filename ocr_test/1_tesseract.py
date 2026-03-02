94a4e077dea0a5e81d759f6d4a84b4e161c633cb
Bohdan Hashchuk <hashchuk.pn@ucu.edu.ua>
2026-03-02 08:18:08 +0200

    added readme

--- ocr_test/1_tesseract.py
+++
@@ -1,8 +0,0 @@
-import pytesseract
-from PIL import Image
-
-image = Image.open('test_silpo.jpg')
-text = pytesseract.image_to_string(image, lang='tessdata/ukr')
-with open('test_silpo.txt', 'w', encoding='utf-8') as text_file:
-    text_file.write(text)
-print(text)
